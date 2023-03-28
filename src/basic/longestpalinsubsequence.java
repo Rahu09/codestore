7bd663834a2010a03322541bd58172c3ab8d0030
RAHU <tiwarirahul0809@gmail.com>
2023-03-28 23:31:55 +0530

    dp string added

--- src/basic/longestpalinsubsequence.java
+++ src/basic/longestpalinsubsequence.java
@@ -1,5 +1,4 @@
 package basic;
-
 import java.util.*;
 
 public class longestpalinsubsequence {
@@ -7,30 +6,23 @@ public class longestpalinsubsequence {
         System.out.println(longestPalindromeSubseq("bbbab"));
     }
     public static int longestPalindromeSubseq(String s) {
-        StringBuffer r = new StringBuffer(s);
-        r.reverse();
-        String rr = r.toString();
+        String r = "";
         int l = s.length();
-        int dp[][]=new int[l][l];
-        for(int rows[]: dp)
-            Arrays.fill(rows,-1);
+        for(int i = 0 ; i<l ; i++) r=s.charAt(i)+r;
 
-        return lcsUtil(s, rr, l-1, l-1, dp);
+        int dp[][]=new int[l][l];
+        for(int rows[]: dp) Arrays.fill(rows,-1);
+
+        return lcsUtil(s, r, l-1, l-1, dp);
     }
     public static int lcsUtil(String s1, String s2, int ind1, int ind2, int[][] dp){
 
-        if(ind1<0 || ind2<0)
-            return 0;
+        if(ind1<0 || ind2<0) return 0;
 
-        if(dp[ind1][ind2]!=-1)
-            return dp[ind1][ind2];
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
 
-        if(s1.charAt(ind1) == s2.charAt(ind2))
-            return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
+        if(s1.charAt(ind1) == s2.charAt(ind2)) return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
 
-        else
-            return dp[ind1][ind2] = 0 + Math.max(lcsUtil(s1,s2,ind1,ind2-1,dp),
-                    lcsUtil(s1,s2,ind1-1,ind2,dp));
-
+        else return dp[ind1][ind2] = Math.max(lcsUtil(s1, s2, ind1, ind2 - 1, dp), lcsUtil(s1, s2, ind1 - 1, ind2, dp));
     }
 }
