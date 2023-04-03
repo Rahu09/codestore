51b112397c6d31f7c15a612d62c36528b91b7f6a
RAHU <tiwarirahul0809@gmail.com>
2023-04-04 04:13:33 +0530

    stacks leetcode que 20. Valid Parentheses added

---
+++ src/stacksQueues/ValidParentheses.java
@@ -0,0 +1,28 @@
+package stacksQueues;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == ')'){
+                if(!stack.isEmpty() && stack.peek() == '(')stack.pop();
+                else return false;
+            }else if(ch == '}'){
+                if(!stack.isEmpty() && stack.peek() == '{')stack.pop();
+                else return false;
+            }else if(ch == ']'){
+                if(!stack.isEmpty() && stack.peek() == '[')stack.pop();
+                else return false;
+            }else stack.push(ch);
+        }
+        return stack.isEmpty();
+    }
+}
